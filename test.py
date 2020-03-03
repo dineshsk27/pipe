997c4075d681ca6a43dbb52d8ab7979bdffb6475
dineshsk27 <52140056+dineshsk27@users.noreply.github.com>
2020-03-03 20:44:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 import boto3
-ec2 = boto3.resource('ec2')
 
-# create a new EC2 instance
-instances = ec2.create_instances(
-     ImageId='ami-0e38b48473ea57778',
-     InstanceType='t2.micro',
-     KeyName='demo'
- )
+client = boto3.client('ec2', region_name='ap-southeast-2')
+
+response = client.run_instances(
+    BlockDeviceMappings=[
+        {
+            'DeviceName': '/dev/xvda',
+            'Ebs': {
+
+                'DeleteOnTermination': True,
+                'VolumeSize': 8,
+                'VolumeType': 'gp2'
+            },
+        },
+    ],
+    ImageId='ami-0e38b48473ea57778',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1,
+    Monitoring={
+        'Enabled': False
+    },
+    SecurityGroupIds=[
+        'sg-096aac8d8bb27a40b',
+    ],
+)
